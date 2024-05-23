0baafc332e9a619897f4f87311690f925e6e18f1
agos <agosnoriega2018@gmail.com>
2024-05-23 19:19:24 -0300

    implementacion de emailjs para los formularios de pedidos y contactos

--- src/Components/MotivoOracion/MotivoOracion.jsx
+++ src/Components/MotivoOracion/MotivoOracion.jsx
@@ -1,67 +1,102 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./motivos.css";
-import Swal from "sweetalert2";
-
-function MotivoOracion() {
-  const navigate = useNavigate();
-
-  const [formData, setFormData] = useState({
-    nombre: "",
-    email: "",
-    telefono: "",
-    pedido: "",
-  });
-
-  const [errors, setErrors] = useState([]);
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleEnviar = async () => {
-    try {
-      if (!formData.nombre || !formData.email || !formData.telefono) {
-        Swal.fire({
-          icon: "error",
-          title: "Error de validación",
-          text: "Por favor complete todos los campos, son obligatorios.",
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
+export const MotivoOracion = () => {
+  /* este codigo se conecta  a la base de datos para recibir los pedidos */
+  /*
+    const navigate = useNavigate();
+  
+    const [formData, setFormData] = useState({
+      nombre: "",
+      email: "",
+      telefono: "",
+      pedido: "",
+    });
+  
+    const [errors, setErrors] = useState([]);
+  
+    const handleInputChange = (e) => {
+      const { name, value } = e.target;
+      setFormData({ ...formData, [name]: value });
+    };
+  
+    const handleEnviar = async () => {
+      try {
+        if (!formData.nombre || !formData.email || !formData.telefono) {
+          Swal.fire({
+            icon: "error",
+            title: "Error de validación",
+            text: "Por favor complete todos los campos, son obligatorios.",
+          });
+          return;
+        }
+        const response = await fetch("http://localhost:3500/motivos", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            id_motivo: 1,
+            nombre: formData.nombre,
+            email: formData.email,
+            telefono: formData.telefono,
+            pedido: formData.pedido,
+          }),
         });
-        return;
+  
+        if (response.ok) {
+          Swal.fire({
+            icon: "success",
+            title: "Mensaje enviado",
+            text: "Tu motivo deoración ha sido enviado correctamente.",
+          });
+          navigate("/");
+        } else {
+          const data = await response.json();
+          setErrors(data.errors || []);
+        }
+      } catch (error) {
+        console.error("Error al enviar el mensaje", error);
       }
-      const response = await fetch("http://localhost:3500/motivos", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-          id_motivo: 1,
-          nombre: formData.nombre,
-          email: formData.email,
-          telefono: formData.telefono,
-          pedido: formData.pedido,
-        }),
+    };*/
+  const refForm = useRef();
+
+  const handleSubmitMotivos = (event) => {
+    event.preventDefault();
+
+    const serviceIdMotivos = 'service_f83zyjj';
+    const templateIdMotivos = 'template_73di0lh';
+    const apiKey = 'GAXjvuQsYAV_fuH47';
+
+    emailjs.sendForm(serviceIdMotivos, templateIdMotivos, refForm.current, apiKey)
+      .then(result => {
+        console.log(result.text);
+
+        refForm.current.reset();
+
+        Swal.fire({
+          title: "Mensaje enviado!",
+          text: "Gracias por contactarnos.",
+          icon: "success",
+          confirmButtonText: "OK"
+        })
+      })
+      .catch(error => {
+        console.error(error);
+
+        Swal.fire({
+          title: "Error!",
+          text: "Hubo un problema al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.",
+          icon: "error",
+          confirmButtonText: "OK"
+        });
       });
 
-      if (response.ok) {
-        Swal.fire({
-          icon: "success",
-          title: "Mensaje enviado",
-          text: "Tu motivo deoración ha sido enviado correctamente.",
-        });
-        navigate("/");
-      } else {
-        const data = await response.json();
-        setErrors(data.errors || []);
-      }
-    } catch (error) {
-      console.error("Error al enviar el mensaje", error);
-    }
-  };
+  }
 
   return (
     <div>
       <div className="contact-container">
-        {errors.length > 0 && (
+        {/*  {errors.length > 0 && (
           <ul className="error-list">
             {errors.map((error, index) => (
               <li key={index} className="error-item">
@@ -69,7 +104,7 @@ function MotivoOracion() {
               </li>
             ))}
           </ul>
-        )}
+        )} */}
         <div className="text-container">
           <div className="text-container">
             {/*  <p>
@@ -84,56 +119,56 @@ function MotivoOracion() {
         </div>
         <div className="form-container">
           <h2>Deja tus Motivos de Oración</h2>
-          <form className="contact-form">
-            <div>
-              <label>Nombre:</label>
+          <form ref={refForm} onSubmit={handleSubmitMotivos} action="" className="contact-form">
+            <fieldset>
+              <label className="name" htmlFor="">Nombre:</label>
               <input
                 type="text"
-                name="nombre"
+                name="name"
                 placeholder="Su nombre"
-                onChange={handleInputChange}
+              /*  onChange={handleInputChange} */
               />
-            </div>
-            <div>
-              <label>Email:</label>
+            </fieldset>
+            <fieldset>
+              <label className="email" htmlFor="">Email:</label>
               <input
                 type="email"
                 name="email"
                 placeholder="Su correo"
-                onChange={handleInputChange}
+              /*  onChange={handleInputChange} */
               />
-            </div>
-            <div>
-              <label>Teléfono:</label>
+            </fieldset>
+            <fieldset>
+              <label className="telefono" htmlFor="">Teléfono:</label>
               <input
                 type="tel"
                 name="telefono"
                 placeholder="999-999999"
-                onChange={handleInputChange}
-                onKeyPress={(e) => {
-                  const pattern = /[0-9-]/;
-                  if (!pattern.test(e.key)) {
-                    e.preventDefault();
-                  }
-                }}
+              /*  onChange={handleInputChange}
+               onKeyPress={(e) => {
+                 const pattern = /[0-9-]/;
+                 if (!pattern.test(e.key)) {
+                   e.preventDefault();
+                 }
+               }} */
               />
-            </div>
-            <div>
-              <label>Mensaje:</label>
+            </fieldset>
+            <fieldset>
+              <label className="pedido" htmlFor="">Mensaje:</label>
               <textarea
                 id="pedido"
                 name="pedido"
                 placeholder="Escribe tu mensaje aquí..."
                 cols="40"
                 rows="6"
-                onChange={handleInputChange}
+              /*  onChange={handleInputChange} */
               ></textarea>
-            </div>
-            <div className="boton">
-              <button type="button" onClick={handleEnviar}>
+            </fieldset>
+          
+              <button type="btn" >
                 Enviar
               </button>
-            </div>
+       
           </form>
         </div>
       </div>
--- src/Components/contacto/Contacto.jsx
+++ src/Components/contacto/Contacto.jsx
@@ -1,9 +1,44 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 import "./Contacto.css";
-import Swal from "sweetalert2";
+import React, { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Swal from 'sweetalert2';
 
-function Contacto() {
+export const  Contacto =()=> {
+  const refForm = useRef();
+
+  const handleSubmitContacto = (event) => {
+    event.preventDefault();
+
+    const serviceIdContacto = 'service_f83zyjj';
+    const templateIdContacto = 'template_m2qg3ul';
+    const apiKey = 'GAXjvuQsYAV_fuH47';
+
+    emailjs.sendForm(serviceIdContacto, templateIdContacto, refForm.current, apiKey)
+      .then(result => {
+        console.log(result.text);
+
+        refForm.current.reset();
+
+        Swal.fire({
+          title: "Mensaje enviado!",
+          text: "Gracias por contactarnos.",
+          icon: "success",
+          confirmButtonText: "OK"
+        })
+      })
+      .catch(error => {
+        console.error(error);
+
+        Swal.fire({
+          title: "Error!",
+          text: "Hubo un problema al enviar el mensaje. Por favor, inténtelo de nuevo más tarde.",
+          icon: "error",
+          confirmButtonText: "OK"
+        });
+      });
+
+  }
+ /*  
   const navigate = useNavigate();
 
   const [formData, setFormData] = useState({
@@ -66,7 +101,7 @@ function Contacto() {
         text: "Error al enviar el mensaje. Por favor, inténtelo de nuevo.",
       });
     }
-  };
+  }; */
 
   return (
     <div>
@@ -76,63 +111,63 @@ function Contacto() {
             <h2>Ponte en Contacto</h2>
           </div>
           <div>
-            <form className="contact-form">
+            <form  ref={refForm} onSubmit={handleSubmitContacto} className="contact-form">
               <div>
-                <label htmlFor="name_user">Nombre:</label>
+                <label className="name_user" htmlFor="">Nombre:</label>
                 <input
                   type="text"
                   id="name_user"
                   name="name_user"
                   placeholder=" Su nombre"
-                  value={formData.name_user}
-                  onChange={handleInputChange}
+                   /*value={formData.name_user}
+                 onChange={handleInputChange}*/
                 />
               </div>
               <div>
-                <label htmlFor="email">Email:</label>
+                <label className="email_user" htmlFor="">Email:</label>
                 <input
                   type="email"
                   id="email"
-                  name="email"
+                  name="email_user"
                   placeholder="Su correo"
-                  value={formData.email}
-                  onChange={handleInputChange}
+                  /*value={formData.email}
+                  onChange={handleInputChange}*/
                 />
               </div>
               <div>
-                <label htmlFor="telefono">Telefono:</label>
+                <label className="telefono_user" htmlFor="">Telefono:</label>
                 <input
                   type="tel"
                   id="telefono"
-                  name="telefono"
+                  name="telefono_user"
                   placeholder="999-999999"
-                  value={formData.telefono}
+                  /*value={formData.telefono}
                   onChange={handleInputChange}
                   onKeyPress={(e) => {
                     const pattern = /[0-9-]/;
                     if (!pattern.test(e.key)) {
                       e.preventDefault();
                     }
-                  }}
+                  }}*/
                 />
               </div>
               <div>
-                <label htmlFor="mensaje">Mensaje:</label>
+                <label className="contacto" htmlFor="">Mensaje:</label>
                 <textarea
                   id="mensaje"
-                  name="mensaje"
+                  name="contacto"
                   placeholder="Escribe tu mensaje aquí..."
                   cols="40"
                   rows="6"
-                  value={formData.mensaje}
-                  onChange={handleInputChange}
+                  /*value={formData.mensaje}
+                  onChange={handleInputChange}*/
                 ></textarea>
               </div>
-              <div className="boton">
-                <button type="submit" onClick={handleEnviar}>
+
+                <button type="btn" >
                   Enviar
                 </button>
-              </div>
+            
             </form>
           </div>
         </div>
